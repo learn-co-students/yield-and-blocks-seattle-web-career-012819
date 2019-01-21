0e85faba86fb00fe7b0918c9a1a42293a3210ce6
tiefengeist <sahirnambiar@gmail.com>
2019-01-20 16:46:35 -0800

    Finished

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
-
+def hello_t(arr)
+  if block_given?
+    i = 0
+  while i < arr.size
+    yield arr[i]
+    i += 1
 end
 
 # call your method here!
-
+arr
+else
+  puts "Hey! No block was given!"
+end
+end
